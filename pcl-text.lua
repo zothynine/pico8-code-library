677f4e95770430fa9849960bbea93a04a98b61f6
Mario Zoth <mario.zoth@icloud.com>
2019-10-31 20:40:22 +0100

    Implement option shadow
    Only paints shadow when stroke i not defined

--- pcl-text.lua
+++ pcl-text.lua
@@ -1,5 +1,5 @@
 function pcl_text(txt, options)
-  local _center,_chars,_w,_x,_y,_col,_stroke = options and options.center,#txt,#txt*4,(options and options.x) or 0,(options and options.y) or 0,(options and options.col) or 7,(options and options.scol)
+  local _center,_chars,_w,_x,_y,_col,_stroke,_shadow = options and options.center,#txt,#txt*4,(options and options.x) or 0,(options and options.y) or 0,(options and options.col) or 7,(options and options.scol),options and options.shd
 
   if _center then
     _x = (127-_w)/2
@@ -14,6 +14,10 @@ function pcl_text(txt, options)
     print(txt, _x-1 ,_y-1, _stroke)
     print(txt, _x+1 ,_y-1, _stroke)
     print(txt, _x-1 ,_y+1, _stroke)
+  end
+
+  if _shadow and not _stroke then
+    print(txt, _x+1, _y+1, _shadow)
   end
 
   print(txt, _x, _y, _col)
