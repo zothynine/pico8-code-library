9f8653c2c4c4ce0f4b3c073044d66d639daddcdc
Mario Zoth <mario.zoth@icloud.com>
2019-10-31 20:34:35 +0100

    Implement options
    center, stroke, xy position, color

--- pcl-text.lua
+++ pcl-text.lua
@@ -1,2 +1,20 @@
 function pcl_text(txt, options)
+  local _center,_chars,_w,_x,_y,_col,_stroke = options and options.center,#txt,#txt*4,(options and options.x) or 0,(options and options.y) or 0,(options and options.col) or 7,(options and options.scol)
+
+  if _center then
+    _x = (127-_w)/2
+  end
+
+  if _stroke then
+    print(txt, _x-1 ,_y, _stroke)
+    print(txt, _x+1 ,_y, _stroke)
+    print(txt, _x ,_y-1, _stroke)
+    print(txt, _x ,_y+1, _stroke)
+    print(txt, _x+1 ,_y+1, _stroke)
+    print(txt, _x-1 ,_y-1, _stroke)
+    print(txt, _x+1 ,_y-1, _stroke)
+    print(txt, _x-1 ,_y+1, _stroke)
+  end
+
+  print(txt, _x, _y, _col)
 end
